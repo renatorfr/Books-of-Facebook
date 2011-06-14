92b16fd7347baa94cf709de1d912231e154ab354
Renato Freire Ricardo <renatorfr@gmail.com>
2011-06-13 22:54:36 -0300

    first commit: the begging of something

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BooksOfFacebook::Application.config.secret_token = 'dd3463a64d0de146982fe847249ccce7ec5463b8144d8c27bd60f8288c31af3b518627aa4442870780136eb87dabee1bebfd425c8a1125520f58dd1d6256c93d'
